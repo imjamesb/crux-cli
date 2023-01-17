d7062d63b8f44163ab0f4f75eb216cea0d997239
James Bradlee <post@jamesb.no>
2023-01-17 15:18:09 +0100

    updated _deps.ts

--- _deps.ts
+++ _deps.ts
@@ -1,8 +1,7 @@
 // deno.land/std - Standard library
-export { encode } from "https://deno.land/std@0.172.0/encoding/base64.ts";
-export { basename } from "https://deno.land/std@0.172.0/path/mod.ts";
+export { encode } from "https://deno.land/std@0.173.0/encoding/base64.ts";
+export { basename } from "https://deno.land/std@0.173.0/path/mod.ts";
 
-export { spin } from "https://crux.land/spin@0.1.0";
 export { fnv1a } from "https://crux.land/6bxRiQ";
 
 import { Base } from "https://crux.land/base@0.1.0";

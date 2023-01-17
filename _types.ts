be1213e7fa1d79921092052954e9f64d7733a615
GithubIsACockSucker <kinkyfuck@jamesb.no>
2023-01-17 13:21:59 +0100

    Added api request type with authentication

--- _types.ts
+++ _types.ts
@@ -2,3 +2,8 @@ export interface CruxApiRequest {
   /** The base URL to use whilst building the request, defaults to `"https://crux.land/api/"` */
   baseUrl?: string | URL;
 }
+
+export interface CruxAuthenticatedApiRequest extends CruxApiRequest {
+  user: number;
+  secret: string;
+}

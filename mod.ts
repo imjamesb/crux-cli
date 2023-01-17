b011a2c78b26e4bb6e497e7a5099ff98a3442a76
GithubIsACockSucker <kinkyfuck@jamesb.no>
2023-01-17 13:22:11 +0100

    Added re-export for all features.

---
+++ mod.ts
@@ -0,0 +1,6 @@
+export * from "./add.ts";
+export * from "./addFile.ts";
+export * from "./aliasList.ts";
+export * from "./aliasRelease.ts";
+export * from "./aliasRequest.ts";
+export * from "./error.ts";
